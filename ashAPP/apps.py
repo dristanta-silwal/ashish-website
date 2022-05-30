28507a8dbc48eb3adaddf1070a0e308ceac0efc5
Dristanta Silwal <you@example.com>
2022-05-30 18:25:57 +0545

    django add

---
+++ ashAPP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AshappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ashAPP'
---
+++ ashAPP/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from ashAPP import views
+
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("about/", views.aboutme, name="about"),
+    path("mywork/", views.mywork, name="mywork"),
+    path("myblog/", views.myblog, name="myblog"),
+    path("contact/", views.contact, name="contact")
+]
