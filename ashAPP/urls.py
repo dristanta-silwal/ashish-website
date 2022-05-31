946c3046e59752c20cdf84d5f3403eb9b21c267d
Dristanta Silwal <you@example.com>
2022-05-31 16:17:05 +0545

    C:/Program Files/Git/ removed

--- ashAPP/urls.py
+++ ashAPP/urls.py
@@ -3,8 +3,8 @@ from ashAPP import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("about/", views.aboutme, name="about"),
-    path("mywork/", views.mywork, name="mywork"),
-    path("myblog/", views.myblog, name="myblog"),
-    path("contact/", views.contact, name="contact")
+    path("about", views.aboutme, name="about"),
+    path("mywork", views.mywork, name="mywork"),
+    path("myblog", views.myblog, name="myblog"),
+    path("contact", views.contact, name="contact")
 ]
